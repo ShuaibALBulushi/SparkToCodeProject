dd8c41f39d9be26555716a2a38e927557db69dd3
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-06-30 15:24:56 +0400

    Add project files.

---
+++ cSharpApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace cSharpApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### Spark To Code ###");
+            Console.Write("Enter Your Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+        }
+    }
+}
