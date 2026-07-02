31723e918c1733d600f148075b5fdb385378f2cf
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-02 09:04:45 +0400

    Task 1 is solved

--- Task2_Problem_Solving/Program.cs
+++ Task2_Problem_Solving/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             
+             Task 1:
+             Ask the user to enter a starting number, then print a countdown from that number down to 1 using a for loop, and
+             print "Liftoff!" after the loop ends.
+
+             */
+
+            Console.Write("Please Enter the starting Number: ");
+            int snum = int.Parse(Console.ReadLine());
+            
+            for(int i = snum; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Liftoff!");
+
+            //////////////////////////////////////////////////////////////////////////
         }
     }
 }
