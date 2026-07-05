d261a030d75ae9b4bfb0de180096f2a82519b38b
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-05 12:32:02 +0400

    task 1 is solved

--- Task3_Problem_Solving/Program.cs
+++ Task3_Problem_Solving/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             Task 1:
+            Ask the user to enter two numbers, subtract the second from the first, and use a Math function to make sure the
+            result is always displayed as a positive value, no matter the order entered.
+
+             */
+
+            Console.Write("Enter the first number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int val = num2 - num1;
+
+            Console.WriteLine("Difference = " + Math.Abs(val));
         }
     }
 }
