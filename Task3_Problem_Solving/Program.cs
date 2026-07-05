20444c2c257ff4e1af358311e2344c2c8ef86ce4
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-05 12:21:46 +0400

    Task 3 project initiation

---
+++ Task3_Problem_Solving/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task3_Problem_Solving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
