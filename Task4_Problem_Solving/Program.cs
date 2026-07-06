1c2279385d2c877a85778b74441a3de9386198a9
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-06 16:37:17 +0400

    task 2 is solved

--- Task4_Problem_Solving/Program.cs
+++ Task4_Problem_Solving/Program.cs
@@ -9,7 +9,13 @@
             Console.WriteLine("Welcome to Spark to Code, " + message);
         }
 
-       
+        //task 2 function:
+        public static int Square(int num1)
+        {
+            double res = Math.Pow(num1, 2);
+            num1 = Convert.ToInt32(res);
+            return num1;
+        }
 
         static void Main(string[] args)
         {
@@ -21,7 +27,12 @@
             //////////////////////////////////////////
 
 
-            
+            //task 2:
+            Console.Write("Enter the number: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("The square of the number is: " + Square(num));
+         
+
         }
     }
 }
