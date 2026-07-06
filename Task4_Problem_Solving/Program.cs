cdfbb4c585893ee12e9701e311e79724fdd6d265
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-06 16:35:44 +0400

    task 1 is solved

--- Task4_Problem_Solving/Program.cs
+++ Task4_Problem_Solving/Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
+
+        //task 1 function:
+        public static void PrintWelcome(string message) 
+        {
+            Console.WriteLine("Welcome to Spark to Code, " + message);
+        }
+
+       
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // task 1:
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            PrintWelcome(name);
+
+            //////////////////////////////////////////
+
+
+            
         }
     }
 }
