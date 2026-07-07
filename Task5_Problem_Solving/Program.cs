674a0145e2e7dc7bfd0f55e3a1abf9fba31bfca1
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-07 13:29:45 +0400

    task 2 is solved

--- Task5_Problem_Solving/Program.cs
+++ Task5_Problem_Solving/Program.cs
@@ -22,6 +22,34 @@
             {
                 Console.WriteLine("Grade: " + grade);
             }
+
+            //////////////////////////////////////////////////////
+
+
+
+            /*
+             Task 2:
+             Create a List<string> to store to-do items. Use a loop to ask the user to enter 5 tasks, adding each one to the list
+             with .Add, then print the full list using foreach.
+             */
+
+            List<string> toDo = new List<string>();
+
+            int taskCount = 5;
+
+            for(int i = 0; i < taskCount; i++) 
+            { 
+                Console.Write("Enter task " + (i + 1) + ": ");
+                toDo.Add(Console.ReadLine());
+            }
+
+            int taskNumber = 1;
+            foreach (string task in toDo)
+            {
+                Console.WriteLine(taskNumber + ". " + task);
+                taskNumber++;
+            }
         }
     }
+    
 }
