c188f056e458c45e7c4ac270fa569fafc5ed5886
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-07 13:20:54 +0400

    task 1 is solved

--- Task5_Problem_Solving/Program.cs
+++ Task5_Problem_Solving/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             Task 1:
+             Declare an array of 5 integers to store student grades. Use a for loop to ask the user to enter each grade and store it
+             in the array, then use a foreach loop to print every grade.
+             */
+
+            int[] grades = new int[5];
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter the grade of student " + (i + 1) + ": ");
+                grades[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach(int grade in grades)
+            {
+                Console.WriteLine("Grade: " + grade);
+            }
         }
     }
 }
