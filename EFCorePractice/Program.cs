a0688cd55e30ea0f6ec8325d544ed09b40fa3f08
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-25 09:43:02 +0400

    EFCore project initiation with its packages

---
+++ EFCorePractice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCorePractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
