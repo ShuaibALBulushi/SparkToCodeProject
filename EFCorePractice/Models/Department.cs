8ae20d2859fcd7faf9262d6a2eef010341f6dd77
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-25 09:51:01 +0400

    added 2 models(employee,department)

---
+++ EFCorePractice/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCorePractice.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public int DepartmentNumber { get; set; }
+    }
+}
---
+++ EFCorePractice/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCorePractice.Models
+{
+    public class Employee
+    {
+        public int EmployeeId {  get; set; }
+        public string Ssn { get; set; }
+        public string EmployeeAddress { get; set; }
+        public double EmplyeeSalary { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeSex { get; set; }
+        public DateTime EmployeeBDate { get; set; }
+
+    }
+}
