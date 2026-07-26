7f0191086793fca8f50701e13dc08812b0c6c455
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-26 09:43:09 +0400

    added models(dependent, project)

---
+++ EFCorePractice/Models/Dependent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCorePractice.Models
+{
+    public class Dependent
+    {
+        public string DependentName { get; set; }
+        public string DependentSex { get; set; }
+        public string DependentBirthDate { get; set; }
+        public string DependentRelationship { get; set; }
+    }
+}
---
+++ EFCorePractice/Models/Project.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCorePractice.Models
+{
+    public class Project
+    {
+        public int ProjecutorNumber { get; set; }
+        public string ProjecutorName { get; set; }
+        public string ProjectLocation { get; set; }
+    }
+}
