e4e843ee78b52b5799833804d8d917378dfd79f4
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-26 10:58:28 +0400

    add migration, update database are done

--- EFCorePractice/Migrations/20260726065336_intialCreate.Designer.cs
+++ EFCorePractice/Migrations/20260726065336_intialCreate.Designer.cs
@@ -4,6 +4,7 @@ using EFCorePractice;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EFCorePractice.Migrations
 {
     [DbContext(typeof(ProjectContext))]
-    partial class ProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20260726065336_intialCreate")]
+    partial class intialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EFCorePractice/Migrations/20260726065336_intialCreate.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCorePractice.Migrations
+{
+    /// <inheritdoc />
+    public partial class intialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "departmentLocations",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false),
+                    DeptLocation = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_departmentLocations", x => new { x.DepartmentId, x.DeptLocation });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "departments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DepartmentNumber = table.Column<int>(type: "int", nullable: false),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    ManageStartDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_departments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ssn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmplyeeSalary = table.Column<double>(type: "float", nullable: false),
+                    EmployeeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeSex = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeBDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false),
+                    SupervisorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employees", x => x.EmployeeId);
+                    table.ForeignKey(
+                        name: "FK_employees_departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "departments",
+                        principalColumn: "DepartmentId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_employees_employees_SupervisorId",
+                        column: x => x.SupervisorId,
+                        principalTable: "employees",
+                        principalColumn: "EmployeeId",
+                        onDelete: ReferentialAction.NoAction,
+                        onUpdate: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "projects",
+                columns: table => new
+                {
+                    ProjectNumber = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProjecutorName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProjectLocation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_projects", x => x.ProjectNumber);
+                    table.ForeignKey(
+                        name: "FK_projects_departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "departments",
+                        principalColumn: "DepartmentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "dependents",
+                columns: table => new
+                {
+                    DependentName = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    DependentSex = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DependentBirthDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DependentRelationship = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_dependents", x => new { x.EmployeeId, x.DependentName });
+                    table.ForeignKey(
+                        name: "FK_dependents_employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "employees",
+                        principalColumn: "EmployeeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "employeeProjects",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    ProjectNumber = table.Column<int>(type: "int", nullable: false),
+                    ProjectHours = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employeeProjects", x => new { x.EmployeeId, x.ProjectNumber });
+                    table.ForeignKey(
+                        name: "FK_employeeProjects_employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "employees",
+                        principalColumn: "EmployeeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_employeeProjects_projects_ProjectNumber",
+                        column: x => x.ProjectNumber,
+                        principalTable: "projects",
+                        principalColumn: "ProjectNumber",
+                        onDelete: ReferentialAction.NoAction,
+                        onUpdate: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_departments_EmployeeId",
+                table: "departments",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_dependents_EmployeeId",
+                table: "dependents",
+                column: "EmployeeId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_employeeProjects_ProjectNumber",
+                table: "employeeProjects",
+                column: "ProjectNumber");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_employees_DepartmentId",
+                table: "employees",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_employees_SupervisorId",
+                table: "employees",
+                column: "SupervisorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_projects_DepartmentId",
+                table: "projects",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_departmentLocations_departments_DepartmentId",
+                table: "departmentLocations",
+                column: "DepartmentId",
+                principalTable: "departments",
+                principalColumn: "DepartmentId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_departments_employees_EmployeeId",
+                table: "departments",
+                column: "EmployeeId",
+                principalTable: "employees",
+                principalColumn: "EmployeeId",
+                onDelete: ReferentialAction.NoAction,
+                onUpdate: ReferentialAction.NoAction);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_employees_departments_DepartmentId",
+                table: "employees");
+
+            migrationBuilder.DropTable(
+                name: "departmentLocations");
+
+            migrationBuilder.DropTable(
+                name: "dependents");
+
+            migrationBuilder.DropTable(
+                name: "employeeProjects");
+
+            migrationBuilder.DropTable(
+                name: "projects");
+
+            migrationBuilder.DropTable(
+                name: "departments");
+
+            migrationBuilder.DropTable(
+                name: "employees");
+        }
+    }
+}
