2470c9f3c7c3c9b91d38ed5708123efb7d11076e
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-27 11:33:56 +0400

    added key

--- Task9_Problem_Solving/Models/Review.cs
+++ Task9_Problem_Solving/Models/Review.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
@@ -7,6 +8,7 @@ namespace Task9_Problem_Solving.Models
 {
     public class Review
     {
+        [Key]
         public int ReviewId { get; set; }
         public double Rating { get; set; }
         public string Comment { get; set; }
