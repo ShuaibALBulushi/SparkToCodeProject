b5a2227a6c468fbcf792fcf114c28a9e4285c6da
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-27 11:15:57 +0400

    Review class added

---
+++ Task9_Problem_Solving/Models/Review.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task9_Problem_Solving.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public double Rating { get; set; }
+        public string Comment { get; set; }
+    }
+}
