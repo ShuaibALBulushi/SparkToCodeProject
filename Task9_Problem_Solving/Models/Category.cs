c1eaf17fbc6713b4b2be6296084615bb8496f5ef
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-27 11:34:46 +0400

    added key

--- Task9_Problem_Solving/Models/Category.cs
+++ Task9_Problem_Solving/Models/Category.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Task9_Problem_Solving.Models
 {
     public class Category
     {
+        [Key]
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
 
--- Task9_Problem_Solving/Models/User.cs
+++ Task9_Problem_Solving/Models/User.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Task9_Problem_Solving.Models
 {
     public class User
     {
+        [Key]
         public int UserId { get; set; }
         public string UserName { get; set; }
         public string UserPassword { get; set; }
