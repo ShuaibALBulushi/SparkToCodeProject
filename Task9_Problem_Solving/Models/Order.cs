cba4bedaa5cad7a65bbb13ead88b2062d8b2ce88
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-27 11:11:33 +0400

    Order class added

---
+++ Task9_Problem_Solving/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task9_Problem_Solving.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
