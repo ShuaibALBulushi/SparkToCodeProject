583dfa6f46124d42dd52bd2cc83e739b2cf3d886
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-27 11:08:28 +0400

    Product class added

---
+++ Task9_Problem_Solving/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task9_Problem_Solving.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string PublicName { get; set; }
+        public double ProductPrice { get; set; }
+    }
+}
