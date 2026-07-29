5c8cd64e565ea324890fbf5c732ffac8e294f9d0
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-29 13:34:49 +0400

    added models(employee, department)

---
+++ WebAPIPractice/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace WebAPIPractice.Models
+{
+    public class Department
+    {
+        [Key]
+        [JsonIgnore]
+        public int DepartmentId { get; set; }
+        [Required]
+        public string DepartmentName { get; set; }
+        [Required]
+        public int DepartmentNumber { get; set; }
+
+        //work for relationship 1-M
+        [JsonIgnore]
+        public List<Employee>? Employee { get; set; }
+    }
+}
---
+++ WebAPIPractice/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace WebAPIPractice.Models
+{
+    public class Employee
+    {
+        [Key]
+        [JsonIgnore]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public string EmployeeAddress { get; set; }
+
+        [Required]
+        public double EmplyeeSalary { get; set; }
+
+        [Required]
+        public string EmployeeName { get; set; }
+
+        [Required]
+        public string EmployeeGender { get; set; }
+
+        //work for relationship 1-M
+        [ForeignKey("department")]
+        [Required]
+        public int DepartmentId { get; set; }
+
+        [JsonIgnore]
+        public Department? department { get; set; }
+    }
+}
