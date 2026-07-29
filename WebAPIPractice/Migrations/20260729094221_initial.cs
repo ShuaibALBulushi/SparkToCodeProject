e243366dd2d399610faa97a62a0aea8cd626dd4d
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-29 13:45:59 +0400

    created db connection and used(add-migration, update-database)

---
+++ WebAPIPractice/Migrations/20260729094221_initial.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPIPractice.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "departments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DepartmentNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_departments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmplyeeSalary = table.Column<double>(type: "float", nullable: false),
+                    EmployeeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeGender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employees", x => x.EmployeeId);
+                    table.ForeignKey(
+                        name: "FK_employees_departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "departments",
+                        principalColumn: "DepartmentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_employees_DepartmentId",
+                table: "employees",
+                column: "DepartmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "employees");
+
+            migrationBuilder.DropTable(
+                name: "departments");
+        }
+    }
+}
---
+++ WebAPIPractice/ProjectContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIPractice.Models;
+
+namespace WebAPIPractice
+{
+    public class ProjectContext : DbContext
+    {
+        DbSet<Employee> employees { get; set; }
+        DbSet<Department> departments { get; set; }
+
+        public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
+        {
+        }
+    }
+}
