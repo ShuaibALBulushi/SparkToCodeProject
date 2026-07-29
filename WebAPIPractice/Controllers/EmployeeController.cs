4c4c212fe21edf5450c81ba85d2c2cd2cc2253b2
Shuaib Al-Bulushi <shuaib1151@gmail.com>
2026-07-29 14:00:13 +0400

    fixed access issue and added first  service in employee controller

---
+++ WebAPIPractice/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.AccessControl;
+using WebAPIPractice.Models;
+
+namespace WebAPIPractice.Controllers
+{
+    [ApiController]
+    [Route("Employee")]
+    public class EmployeeController : ControllerBase
+    {
+        private ProjectContext context;
+
+        public EmployeeController(ProjectContext _context)
+        {
+            context = _context;
+        }
+
+        [HttpPost("AddEmployee")]
+        public IActionResult AddEmployee(Employee emp)
+        {
+            context.employees.Add(emp);
+            context.SaveChanges();
+
+            return Ok(emp.EmployeeId);
+        }
+
+    }
+}
--- WebAPIPractice/ProjectContext.cs
+++ WebAPIPractice/ProjectContext.cs
@@ -5,8 +5,8 @@ namespace WebAPIPractice
 {
     public class ProjectContext : DbContext
     {
-        DbSet<Employee> employees { get; set; }
-        DbSet<Department> departments { get; set; }
+        public DbSet<Employee> employees { get; set; }
+        public DbSet<Department> departments { get; set; }
 
         public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
         {
